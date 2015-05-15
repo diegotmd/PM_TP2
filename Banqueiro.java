9e396183a9f6103d7c10177facf0273ae592cc83
diegotmd <diegotmd@gmail.com>
2015-05-15 14:11:36 -0300

    Commit inicial

---
+++ Banqueiro.java
@@ -0,0 +1,13 @@
+package br.ufmg.dcc.pm.seteemeio;
+
+/**
+ *
+ * @author ddayrell
+ */
+public class Banqueiro extends Jogador {
+
+    public Banqueiro(String nome, double saldoInicial, JogoSeteEMeio jogo) {
+        super(nome, saldoInicial, jogo);
+    }
+    
+}
