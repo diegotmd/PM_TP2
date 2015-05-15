27e9d3e0b3b9cbcad4208705a741b714eac0d9b9
diegotmd <diegotmd@gmail.com>
2015-05-15 14:15:57 -0300

    Commit inicial

---
+++ PM_TP2.java
@@ -0,0 +1,16 @@
+package br.ufmg.dcc.pm.tp2;
+
+/**
+ *
+ * @author ddayrell
+ */
+public class PM_TP2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
